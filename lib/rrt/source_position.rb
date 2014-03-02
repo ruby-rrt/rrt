182fe96114b715e92cf5118f7e113105fd2ba8b6
Yassen Bantchev <yassenb@gmail.com>
2014-03-02 12:33:58 +0200

    Implement source position arithmetic

--- lib/rrt/source_position.rb
+++ lib/rrt/source_position.rb
@@ -7,5 +7,26 @@ module RRT
       return column <=> other_position.column if column != other_position.column
       0
     end
+
+    def +(n)
+      return self.-(-n) if n < 0
+
+      current_line_length = source.lines[line].size
+      if column + n < current_line_length
+        source.position(line, column + n)
+      else
+        source.position(line + 1, 0) + (n - (current_line_length - column))
+      end
+    end
+
+    def -(n)
+      return self.+(-n) if n < 0
+
+      if column - n >= 0
+        source.position(line, column - n)
+      else
+        source.position(line - 1, source.lines[line - 1].size - 1) - (n - (column + 1))
+      end
+    end
   end
 end
--- spec/rrt/source_position_spec.rb
+++ spec/rrt/source_position_spec.rb
@@ -14,12 +14,50 @@ module RRT
       it 'position is less than other if column number is smaller' do
         expect(source_position(0, 0)).to be < source_position(0, 1)
       end
+    end
 
-      private
-
-      def source_position(line, column)
-        described_class.new("line\nline", line, column)
+    describe '#+' do
+      it 'gives the same position when adding 0' do
+        expect(source_position(0, 1) + 0).to eq(source_position(0, 1))
       end
+
+      it 'gives the next position when adding 1' do
+        expect(source_position(0, 1) + 1).to eq(source_position(0, 2))
+      end
+
+      it 'gives the next position when it\'s on a subsequent line' do
+        expect(source_position(0, 1) + 4).to eq(source_position(1, 0))
+        expect(source_position(0, 1) + 6).to eq(source_position(1, 2))
+      end
+
+      it 'works with negative numbers' do
+        expect(source_position(0, 3) + -1).to eq(source_position(0, 2))
+      end
+    end
+
+    describe '#-' do
+      it 'gives the same position when subtracting 0' do
+        expect(source_position(0, 2) - 0).to eq(source_position(0, 2))
+      end
+
+      it 'gives the next position when subtracting 1' do
+        expect(source_position(0, 2) - 1).to eq(source_position(0, 1))
+      end
+
+      it 'gives the previous position when it\'s on a preceding line' do
+        expect(source_position(1, 1) - 2).to eq(source_position(0, 4))
+        expect(source_position(1, 1) - 4).to eq(source_position(0, 2))
+      end
+
+      it 'works with negative numbers' do
+        expect(source_position(0, 1) - -1).to eq(source_position(0, 2))
+      end
+    end
+
+    private
+
+    def source_position(line, column)
+      described_class.new(Source.new("line\nline"), line, column)
     end
   end
 end
